34fc4546490de045b3c2bc9cabf698d2f7baf77c
Chris <lovedknightt@yahoo.com>
2018-08-02 11:44:41 +0000

    added comments and removed debugging scripts

--- SP.js
+++ SP.js
@@ -1,26 +1,32 @@
+//initialize all variables and dependencies for application (Express,mongoose, body-parser, correct path for file usage)
 var express = require("express");
 var app = express();
 var path = require("path");
 var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 
+//handshake for mongo database (useNewURLParser for most current versions)
 mongoose.connect("mongodb://localhost:27017/root_bank", { useNewUrlParser: true });
+//name the path/file location for application resources such as views/scripts/styles
 app.use(express.static(path.join(__dirname, '/public')));
+//dependecy that extends our functionality to grabs HTML elements.
 app.use(bodyParser.urlencoded({extended:true}));
+
+// set the view engine to ejs
 app.set("view engine", "ejs");
 
-//Schema setup
+//Schema setup for storage of roots in database
 var rootBankSchema = new mongoose.Schema({
     searchWord: String,
     rootWord: String
 });
-
+//model for DB object
 var rootBank = mongoose.model("root_bank", rootBankSchema);
 
 
 
 
-
+//attempted array to store search root woord queries
 var root_bank = [
     
     {searchWord: "وبالوالدين",
@@ -29,27 +35,21 @@ var root_bank = [
 
 
 
-
+// GET method that loads our homepage
 app.get("/", function(req, res){
     res.render("SP");
 });
+
+// POST method that can be edited/applied to application when needed. Not 100% functional yet.
 app.post("/", function(req,res){
     var word = req.body.wd;
     var root = req.body.rtWd;
     var newRoot = {searchWord: word, rootWord: root};
     root_bank.push(newRoot);
-    
-  rootBank.find({}, function(err, rootbank){
-      if(err){
-          console.log(err);
-      }else{
-          res.render("SP", {root_bank:rootbank});
-      }
-  });
 });
 
 
-
+//developer tool/notification that our server connected without failure when debugging.
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("!!!!!SERVER ON!!!!!")
 });
--- public/scripts/SP_JS.js
+++ public/scripts/SP_JS.js
@@ -1,6 +1,10 @@
+//grab the button from out page and initialize it to a variable that we can then use to create an event
 const button = document.getElementById("submitBtn");
+
+//dictionary for the arabic definite article "the" or ال
 var ALSHADDA = {
 	"اللّ" : "ّ".charCodeAt(0)};
+//dicionary object for ALL arabic characters 
 var DICT = {
 	"ذ" : "ذ".charCodeAt(0),
 	"ض" : "ض".charCodeAt(0),
@@ -34,6 +38,7 @@ var DICT = {
 	"ز" : "ز".charCodeAt(0),
 	"ظ" : "ظ".charCodeAt(0)
 };
+//dictionary object for the sun letters: letters in the arabic alphabet which, when preceded by the definite article ال , force the word to be pronounced differently. (Aka, we don't pronounce the lam part)
 var SUN = {
 "ن" : "ن".charCodeAt(0),
 "ط" : "ط".charCodeAt(0),
@@ -50,12 +55,14 @@ var SUN = {
 "ض" : "ض".charCodeAt(0),
 "ل" : "ل".charCodeAt(0)
 };
+//dictionary object for characters that algorithm will recognize as things that need to be changed into a different correlated arabic character
 var MUT = {
 	"إ":"إ".charCodeAt(0),
 	"ؤ":"ؤ".charCodeAt(0),
 	"ئ":"ئ".charCodeAt(0),
 	"آ":"آ".charCodeAt(0)
 };
+// TABLE 1 G1 Dictionary
 var G1 = {
 "ز" : "ز".charCodeAt(0),
 "ر" : "ر".charCodeAt(0),
@@ -74,13 +81,16 @@ var G1 = {
 "ص" : "ص".charCodeAt(0),
 "ش" : "ش".charCodeAt(0)
 };
+//Table 1 G2 Dictionary
 var G2 = {
 	"ه" : "ه".charCodeAt(0)
 };
+//TABLE 1 G3 Dictionary
 var G3 = {
 	"ه" : "ه".charCodeAt(0),
 	"ف" : "ف".charCodeAt(0)
 };
+//TABLE 1 G4 Dictionary
 var G4 = {
 	"ه" : "ه".charCodeAt(0),
 	"ف" : "ف".charCodeAt(0),
@@ -93,6 +103,7 @@ var G4 = {
 	"ك" : "ك".charCodeAt(0),
 	"ب" : "ب".charCodeAt(0)
 };
+//TABLE 1 G5 Dictionary
 var G5 = {
 	"ه" : "ه".charCodeAt(0),
 	"ف" : "ف".charCodeAt(0),
@@ -107,6 +118,7 @@ var G5 = {
 	"م" : "م".charCodeAt(0),
 	"س" : "س".charCodeAt(0)
 };
+//TABLE 1 G6 Dictionary
 var G6 = {
 	"ف" : "ف".charCodeAt(0),
 	"ئ" : "ئ".charCodeAt(0),
@@ -117,6 +129,7 @@ var G6 = {
 	"ب" : "ب".charCodeAt(0),
 	"س" : "س".charCodeAt(0)
 };
+//TABLE 1 G7 Dictionary
 var G7 = {
 	"ئ" : "ئ".charCodeAt(0),
 	"ؤ" : "ؤ".charCodeAt(0),
@@ -128,12 +141,14 @@ var G7 = {
 	"ب" : "ب".charCodeAt(0),
 
 };
+//TABLE 8 DICTIONARIES (dual rule for this section of the table, thus split into two dictionaries)
 var G8A = {
 	"ا" : "ا".charCodeAt(0)
 };
 var G8B = {
 	"ة" : "ة".charCodeAt(0)
 };
+//extra rule that was not applied. Dictionary for future use
 var G9 = {
 	"ئ" : "ئ".charCodeAt(0),
 	"ؤ" : "ؤ".charCodeAt(0),
@@ -144,46 +159,49 @@ var G9 = {
 	"ب" : "ب".charCodeAt(0),
 	"ء" : "ء".charCodeAt(0)
 }
+
+//initialize all of our form elements into variables
 var submit = document.getElementById("submitBtn");
 var WORD = document.getElementById("srchBox").value;
 var results = document.getElementById("results");
+//initiliaze initial variables in accordance with the algorithm
 var countOn = 0;
 var countOff = 0;
 var flag = true;
 var r1_flag = false;
 var rn_flag = false;
 var Changes = false;
+//Split the word into an array.
 var W = WORD.split("");
 var N = WORD.length;
 var R = new Array(WORD.length).fill(-1);
 var ROOT = "";
 
-
+//Main method that is called on button click
 function mainMethod(){
+//redefine our word to current search box value, and apply the definite artcile method that checks for ال, and then define N as the length of the word
 WORD = document.getElementById("srchBox").value;
 defArticle();
 N=WORD.length;
-console.log(countOn);
-console.log(WORD);
-console.log(R);
+// the algorithm calls to check the word again for a second index of  ال, and if there is another instance of these characters in the word then we should recognize a root letter from that.
 if(WORD.includes("ال")){
 	R[WORD.indexOf("ال")+1] = 1;
 }
-console.log(countOn);
-console.log(WORD);
-console.log(R);
+// per our algorithm, initialized second set of variabels necessary for the extraction
 var n = N;
 var S1 = WORD.substring(0,(Math.round(n/3)));
 var S2 = WORD.substring(Math.round(n/3),Math.round(n/3) +(n-(2*S1.length)));
 var S3 = WORD.substring(S1.length + S2.length);
 var Wseg = [S1, S2, S3];
 W = WORD.split("");
+
+//apply the mutate function to the word, using word and word segment variables
 mutateFunc(S1, S2, S3, W);
-console.log(WORD);
-console.log(R);
+
+//grab all values from the object G1 and put them in vals variable
 var vals = Object.values(G1);
 
-console.log(vals);
+//loop through vals to apply the Table 1 rules
 for(var i=0; i<vals.length; i++){
 	for(var j=0; j<WORD.length; j++){
 		if(WORD.charCodeAt(j) == vals[i]){
@@ -192,8 +210,8 @@ for(var i=0; i<vals.length; i++){
 		}
 	}
 }
-console.log(WORD);
-console.log(R);
+
+//redefine vals as the values from the G8A object and loop through the word segment S1 to check against these values
 vals = Object.values(G8A);
 for(var j = 0; j<S1.length; j++){
 	if(S1.charCodeAt(j) == vals[i]){
@@ -201,17 +219,17 @@ for(var j = 0; j<S1.length; j++){
 		countOff++;
 	}
 }
-console.log(WORD);
-console.log(R);
+
+//redefine vals as G8B values and check if they occurr at the end of the word segment 3
 vals = Object.values(G8B);
 if(S3.endsWith(vals[i])){
 	R[WORD.indexOf(vals[i])] = 0;
 	countOff++;
 }
-console.log(WORD);
-console.log(R);
 
-console.log(countOn);
+
+//apply if statements outlined in algorithm. If the countOn variable that is counting how many root letters have been found is more than 3, create a ROOT string from those found values and print it to screen. Return to end the method and 
+//stop extraction
 var st_in = 1;
 var ls_in = n;
 if (countOn >= 3){
@@ -220,20 +238,15 @@ if (countOn >= 3){
 			ROOT = ROOT.concat(WORD.charAt(i));
 		}
 	}
-	console.log(R);
-	console.log(ROOT);
 	document.getElementById("rsltBox").innerHTML = ROOT;
 	return;
 }
+//if we've found two root letters, apply the rootDistance method to check if our word can be shortened for a smaller search paremeter.
 if(countOn == 2 && flag){
 rootDistance(R);
-console.log(WORD);
-console.log(R);
 }
 
-console.log(n);
-console.log(WORD);
-console.log(R);
+//apply if else statements as per outlined by algorithm
 if(R[st_in] == 0 || R[ls_in] == 0){
 	if(R[st_in] == 0){
 		r1_flag = true;
@@ -248,10 +261,10 @@ if(R[st_in] == 0 || R[ls_in] == 0){
 		}
 	}
 }
-
+//redfine our n and word segments.
 n=WORD.length;
 Wseg = [S1, S2, S3];
-
+//initialize all variables that correlate to our TABLE 1 dictionaries
 vals = Object.values(G2);
 var var3 = Object.values(G3);
 var var4 = Object.values(G4);
@@ -259,17 +272,13 @@ var var5 = Object.values(G5);
 var var6 = Object.values(G6);
 var var7 = Object.values(G7);
 var var9 = Object.values(G9);
-console.log(Wseg);
-console.log(vals + " " +var3 + " " +var4 + " " +var5 + " " +var6 + " " +var7);
-console.log(WORD);
-console.log(R);
 
+//TABLE 1 G2 rules
 if(S1.charCodeAt(0) == vals[0]){
 	R[0] = 1;
 	countOn++;
 }
-console.log(WORD);
-console.log(R);
+//TABLE 1 G3 RULES
 for(var i=0; i<var3.length; i++){
 	for(var j = 0; j<S1.length; j++){
 		if(S1.charCodeAt(j) == var3[i]){
@@ -278,17 +287,14 @@ for(var i=0; i<var3.length; i++){
 		}
 	}
 }
-console.log(WORD);
-console.log(R);
-console.log(S2);
+//TABLE 1 G4 RULES
 for(var i=0; i<var4.length; i++){
 	if(S2.charCodeAt(0) == var4[i]){
 		R[S1.length] = 1;
 		countOn++;
 	}
 }
-console.log(WORD);
-console.log(R);
+//TABLE 1 G5 RULES
 for(var i=0; i<var5.length; i++){
 	for(var j = 0; j<S2.length; j++){
 		if(S2.charCodeAt(j) == var5[i]){
@@ -297,16 +303,14 @@ for(var i=0; i<var5.length; i++){
 		}
 	}
 }
-console.log(WORD);
-console.log(R);
+//TABLE 1 G6 RULES
 for(var i=0; i<var6.length; i++){
 	if(S2.charCodeAt(S2.length-1) == var6[i]){
 		R[S1.length+S2.length+1] = 1;
 		countOn++;
 	}
 }
-console.log(WORD);
-console.log(R);
+//TABLE 1 G7 RULES
 for(var i=0; i<var7.length; i++){
 	for (var j = 0; j<S3.length; j++){
 		if(WORD.charCodeAt(n-1) == var7[i]){
@@ -315,44 +319,27 @@ for(var i=0; i<var7.length; i++){
 		}
 	}
 }
-for(var i=0; i<var9.length; i++){
-	for (var j = 0; j<S3.length; j++){
-		if(S3.charCodeAt(j) == var7[i]){
-			R[S1.length+S2.length+j] = 1;
-			countOn++;
-		}
-}
-}
-console.log(WORD);
-console.log(R);
+// ALGORITHM implementation that handles letters not mentioned in TABLE 1 and outputs our final word after extraction
 while(Changes == false){
-	console.log(Changes);
 	extraRules(S1, S2, S3, n);
 	if(Changes){
-		console.log(countOn);
 		if (countOn >= 3){
 			for(var i = 0; i<R.length+1; i++){
 				if (R[i]==1){
 					ROOT = ROOT.concat(WORD.charAt(i));
-					console.log(WORD);
 				}
 			}
-	console.log(R);
-	console.log(ROOT);
 	document.getElementById("rsltBox").innerHTML = ROOT;
 	return;
 		}
 	
 	}
 }
-console.log(R);
-console.log("after");
-console.log(Changes);
 };
 
 
 
-
+//function that searchs word for occurence of ال, finds the index, and then applies 4 rules depending on where the index begins and what letters are neighboring the occurence of ال
 function defArticle(){
 	if(WORD.includes("ال")){
 		var index = WORD.indexOf("ال");
@@ -484,7 +471,7 @@ function defArticle(){
 		}
 	}
 };
-
+//mutate function that checks our word for specfic instances of specified variables in our MUT object library. If these instances are found, apply 1 of 4 rules to these characters and MUTATE them into related characters.
 function mutateFunc(S1, S2, S3, W){
 	W=W;
 	console.log(W);
@@ -516,7 +503,7 @@ function mutateFunc(S1, S2, S3, W){
 		}
 		}
 };
-
+// function that applies any rules that have not already been applied by one of the previous functions. these rules are based off of character indexes and whether or not roots have already been found.
 function extraRules(s1, s2, s3, N){
 	var S1 = s1;
 	var S2 = s2;
@@ -558,6 +545,7 @@ function extraRules(s1, s2, s3, N){
 		}
 		Changes = true;
 };
+//root distance algorithm that checks how far awy two root letters are if found early on in the extraction
 function rootDistance(R){
 	var r = R;
 
@@ -566,7 +554,7 @@ function rootDistance(R){
 		return WORD;
 	}
 };
-
+//javascript DOM manipulation event that listens for the click of the button and performs our root extraction
 button.addEventListener("click", function(e){
 	mainMethod();
 });
